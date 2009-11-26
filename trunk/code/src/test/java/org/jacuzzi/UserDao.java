33204e31fe673820e7c5d6f8b7c27d14cb7dc768
mirzayanovmr <mirzayanovmr@fca76431-e04f-0410-9c07-61c2d654af30>
2009-11-26 08:21:18 +0000

    Added more tests, insert() now returns void.

--- trunk/code/src/test/java/org/jacuzzi/UserDao.java
+++ trunk/code/src/test/java/org/jacuzzi/UserDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 /** @author Mike Mirzayanov */
 public interface UserDao {
     List<User> findByName(String name);
-    boolean insert(User user);
+    void insert(User user);
     User findOnlyByName(String name);
     boolean delete(User user);
 }
