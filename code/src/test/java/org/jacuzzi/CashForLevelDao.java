5cdf5e841d39913f129a5709e79e80dc9cd08351
Levshunov.D <Levshunov.D@fca76431-e04f-0410-9c07-61c2d654af30>
2011-09-27 07:49:17 +0000

    Multiple insert for tables without auto increment on primary key fixed.

---
+++ code/src/test/java/org/jacuzzi/CashForLevelDao.java
@@ -0,0 +1,10 @@
+package org.jacuzzi;
+
+import org.jacuzzi.core.GenericDao;
+
+/**
+ * @author Dmitry Levshunov (d.levshunov@drimmi.com)
+ */
+public interface CashForLevelDao extends GenericDao<CashForLevel, Long> {
+    // Empty.
+}
