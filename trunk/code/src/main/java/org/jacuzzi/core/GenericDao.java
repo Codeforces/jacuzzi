144bd114411487eef7ef442eeb6924a1ea3e0ee7
sladethe <sladethe@fca76431-e04f-0410-9c07-61c2d654af30>
2013-01-28 13:16:42 +0000

    - added GenericDao.insertQuietly.

--- trunk/code/src/main/java/org/jacuzzi/core/GenericDao.java
+++ trunk/code/src/main/java/org/jacuzzi/core/GenericDao.java
@@ -108,6 +108,33 @@ public interface GenericDao<T, K> {
     void insert(List<T> objects);
 
     /**
+     * Inserts entity instance. It updates instance's @Id field if
+     * insert statement generates exactly one generated key (auto-incremented value).
+     * Never throws exception.
+     *
+     * @param object Entity instance.
+     */
+    void insertQuietly(T object);
+
+    /**
+     * Inserts multiple entity instances. It updates instance's @Id field if
+     * insert statement generates exactly one generated key (auto-incremented value).
+     * Never throws exception.
+     *
+     * @param objects Variable number of entity instances.
+     */
+    void insertQuietly(T... objects);
+
+    /**
+     * Inserts multiple entity instances. It updates instance's @Id field if
+     * insert statement generates exactly one generated key (auto-incremented value).
+     * Never throws exception.
+     *
+     * @param objects List of entity instances.
+     */
+    void insertQuietly(List<T> objects);
+
+    /**
      * Updates entity instance.
      *
      * @param object Entity instance.
